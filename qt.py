3bae1dd548a0ad2102c7455d1992aeb118b47dd9
Ckzzz1 <chenkangzzz23@gmail.com>
2019-04-02 20:43:45 +0800

    提交信息

--- qt.py
+++ qt.py
@@ -4,7 +4,7 @@ from day import Ui_MainWindow
 import requests
 import collector
 
-class MyWindow(QMainWindow, Ui_MainWindow): #调用qt designer生成的界面类并添加查询代码
+class MyWindow(QMainWindow, Ui_MainWindow): #成调用qt designer生的界面类并添加查询代码
     def __init__(self, parent=None):
         super(MyWindow, self).__init__(parent)
         self.ui = Ui_MainWindow()
@@ -12,9 +12,15 @@ class MyWindow(QMainWindow, Ui_MainWindow): #调用qt designer生成的界面类
 
     def firstPyQt5_button_click(self):#按钮点击
         self.ui.textBrowser.clear()
-        str = self.ui.dateEdit.text()       #将日期栏中输入的值赋给 month 和 day
-        month = str[0]
-        day = str[2]
+        str = self.ui.dateEdit.text()    #将日期栏中输入的值赋给 month 和 day
+        if str[1] == '/': #判断月份是否为两位数字
+            month = str[0]
+            day = str[2:4]
+
+        else:
+            month = str[0:2]
+            day = str[2:4]
+
         search = collector.Day(month, day)
         result_dict = search.get_thing()        #通过API接口查询
         for i in result_dict['result']:
